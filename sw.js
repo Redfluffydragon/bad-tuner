5df451bfa49f631c8f75b80d148ce6a5503bb7a7
Redfluffydragon <kderu1420@gmail.com>
2020-05-02 13:20:41 -0600

    Style fixes, const, and don't cache images

--- sw.js
+++ sw.js
@@ -1,23 +1,20 @@
-let APP_PREFIX = 'tuner_';
+const APP_PREFIX = 'tuner_';
 
-let VERSION = '0.0.0.1';
+const VERSION = '0.0.0.1';
 
-let CACHE_NAME = APP_PREFIX + VERSION;
+const CACHE_NAME = APP_PREFIX + VERSION;
 
-let URLS = [
+const URLS = [
   '/tuner/',
   '/tuner/index.html',
-  '/tuner/index.js',
+  '/tuner/min.js',
   '/tuner/index.css',
-  '/tuner/img/apple-touch-icon.png',
-  '/tuner/img/icon.png'
 ];
-
 
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(function (request) {
-      return request || fetch(e.request)
+      return request || fetch(e.request);
     })
   )
 }, false);
@@ -33,17 +30,17 @@ self.addEventListener('install', e => {
 self.addEventListener('activate', e => {
   e.waitUntil (
     caches.keys().then(keyList => {
-      let cacheWhitelist = keyList.filter(key =>{
+      let cacheWhitelist = keyList.filter(key => {
         return key.indexOf(APP_PREFIX);
-      })
+      });
       cacheWhitelist.push(CACHE_NAME);
 
       return Promise.all(keyList.map((key, i) => {
         if (cacheWhitelist.indexOf(key) === -1) {
-          return caches.delete(keyList[i]);
+          return caches.delete(keyList[parseInt(i)]);
         }
-      }))
+      }));
 
     })
-  )
+  );
 }, false);
